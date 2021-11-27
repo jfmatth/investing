7fb6b27feb85caf7d4e761ba6c0c5704bcec0b74
John Matthew <john@compunique.com>
2021-11-27 08:27:11 -0800

    continued signals work...

--- loader/apps.py
+++ loader/apps.py
@@ -1,15 +1,14 @@
 from django.apps import AppConfig
 from django.dispatch import Signal
 
-# from loader.signals import receiverLoadSymbols
-
 
 class LoaderConfig(AppConfig):
     name = 'loader'
 
     def ready(self) -> None:
-        print("app ready")
+        print("Loader app ready")
+        
         import loader.signals
-        print("signal connected")
+        print("Loader signals connected")
 
         return super().ready()
